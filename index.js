359bdb6d227010d0008be77ee52771436264853b
senior9 <ovi215188@gmail.com>
2023-05-21 04:08:49 +0600

    server is working

---
+++ index.js
@@ -0,0 +1,22 @@
+const express =require('express');
+const app = express();
+const port =  process.env.PORT || 5000;
+const  cors = require('cors');
+
+
+// Middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req,res)=>{
+    res.send("Server is Running");
+    console.log("Hello world")
+})
+
+
+
+app.listen(port,()=>{
+    console.log("apps running ")
+})
