561089748e721b3bb71622fc57190c0f98a70636
senior9 <ovi215188@gmail.com>
2023-05-23 12:47:01 +0600

    get data from user

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express =require('express');
 require('dotenv').config()
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port =  process.env.PORT || 5000;
 const  cors = require('cors');
 
@@ -17,7 +17,6 @@ app.get("/", (req,res)=>{
     console.log("Hello world")
 })
 
-console.log(process.env.DB_PASS)
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.yx2s4d1.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -34,10 +33,63 @@ async function run() {
   try {
     await client.connect();
      const carCollection = client.db("carCollection").collection("carData");
+     const addCarCollection =client.db("carCollection").collection("addCarData");
+    
 
      app.get('/collections', async(req,res)=>{
        const mongoCursor = carCollection.find();
        const result= await mongoCursor.toArray();
+       res.send(result);
+      //  console.log(result)
+     })
+  
+     app.get('/collections/:id', async(req,res)=>{
+      const id =req.params.id;
+      const findSingleData ={_id: new ObjectId(id)}
+      const options ={
+        projections:{toy_name:1,subcategory:1,seller:1,price:1,description:1,available_quantity:1,rating:1,subcategory_id:1,picture:1},
+      }
+      const findResult = await carCollection.findOne(findSingleData,options);
+      res.send(findResult);
+     })
+    //  get  database from post 
+     app.get('/new-collections',async(req,res)=>{
+      const cursor = addCarCollection.find()
+      const result = await cursor.toArray();
+      res.send(result);
+     })
+    
+    //  Get email from get method
+     app.get('/my-collections', async(req, res) => {
+      const email = req.query.email;
+      console.log('Email parameter:', req.query.email);
+      console.log(req.query.email);
+      if (email) {
+        const query = { email: email };
+        const cursor = addCarCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+      } else {
+        res.status(400).send("Email parameter is missing.");
+      }
+    });
+    
+
+    //  app.get('/new-collections', async (req, res) => {
+    //   console.log(req.query);
+    //   console.log('Email parameter:', req.query.email);
+    //   console.log(req.query.email);
+    //   const email = req.query.email;
+    //   const query = email ? { email: email } : {};
+    //   const cursor = addCarCollection.find(query);
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // });
+    
+
+     app.post('/new-collections', async(req,res)=>{
+       const newCarData = req.body;
+       const result = await addCarCollection.insertOne(newCarData);
        res.send(result);
      })
     // Send a ping to confirm a successful connection
